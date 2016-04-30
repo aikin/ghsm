7f4305a0e7deb7f56d5bf1cf377c5cde7675bb86
aikin <me@ulaijn.com>
2016-04-30 18:13:25 +0800

    aikin: config webpack config

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,46 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
 module.exports = {
-  entry: {
-    bundle1: './src/main1.js',
-    bundle2: './src//main2.js'
+  devtool: 'inline-source-map',
+  context: path.resolve(__dirname, '..'),
+  devServer:{
+    contentBase: './src',  //静态资源的目录 相对路径,相对于当前路径 默认为当前config所在的目录
+    devtool: 'eval',
+    hot: true,        //自动刷新
+    inline: true,
+    port: 3005
   },
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    './src/index.js'
+  ],
   output: {
-    filename: './dist/[name].js'
+    filename: 'app.js',
+    path: path.join(__dirname, '../dist/js'),
+    publicPath: /dist/
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        include: path.resolve(__dirname, "./src"),
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      loader: 'babel-loader?presets[]=es2015&presets[]=react&presets[]=stage-0&plugins=transform-runtime',
+      include: path.join(__dirname, './src'),
+      exclude: /node_modules/
+    }]
   }
 };
---
+++ app/webpack.dev.server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), config.devServer)
+  .listen(config.devServer.port, 'localhost', (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.info('listening at localhost:' + config.devServer.port);
+    console.info('opening your system browser...');
+    open('http://localhost:' + config.devServer.port + '/webpack-dev-server/');
+  });
